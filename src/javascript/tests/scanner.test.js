dbbe1e4ea6feba36c9934034a4385d7ae970e798
InfosecSamurai <William.B.Shaw@proton.me>
2025-03-29 19:32:26 +0000

    scanner.test.js

--- src/javascript/tests/scanner.test.js
+++ src/javascript/tests/scanner.test.js
@@ -1,1 +1,29 @@
+const { APIScanner } = require('../core/scanner');
+const { makeApiRequest } = require('../core/utils');
 
+jest.mock('../core/utils');
+
+describe('APIScanner', () => {
+  const mockConfig = {
+    sql_injection: {
+      test_payloads: ["test_payload"]
+    }
+  };
+
+  beforeEach(() => {
+    makeApiRequest.mockReset();
+  });
+
+  test('runScan executes vulnerability checks', async () => {
+    makeApiRequest.mockResolvedValue({
+      statusCode: 200,
+      content: "test response"
+    });
+
+    const scanner = new APIScanner("http://test.com", mockConfig);
+    const results = await scanner.runScan();
+    
+    expect(results.vulnerabilities.length).toBeGreaterThan(0);
+    expect(makeApiRequest).toHaveBeenCalled();
+  });
+});
